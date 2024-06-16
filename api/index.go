dc048341dc35d8c943699544c988a1b1d935d832
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-06-16 05:54:56 +0200

    feat: add request proxy to github content

---
+++ api/index.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	e := echo.New()
+	e.GET("/view-source", func(c echo.Context) error {
+		return c.Redirect(http.StatusMovedPermanently, "https://github.com/luxass/assets")
+	})
+
+	e.GET("/*", func(c echo.Context) error {
+		url := c.Request().URL
+
+		if url.Path == "/" {
+			url.Path = "/README.md"
+		}
+
+		branch := url.Query().Get("branch")
+		if branch == "" {
+			branch = "main"
+		}
+
+		rawURL := "https://raw.githubusercontent.com/luxass/assets/" + branch + url.Path
+
+		resp, err := http.Get(rawURL)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		c.Response().Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+
+		_, err = io.Copy(c.Response(), resp.Body)
+		return err
+	})
+
+	e.ServeHTTP(w, r)
+}
--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
 module github.com/luxass/assets
 
-go 1.22.2
+go 1.20
+
+require github.com/labstack/echo/v4 v4.12.0
 
 require (
-	github.com/a-h/templ v0.2.707 // indirect
-	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
