200288e3ad9d2e1a885945c8f3e85add5f20bbbb
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-06-16 05:33:19 +0200

    chore: use go instead of rust

--- webapi/api/main.rs
+++
@@ -1,9 +0,0 @@
-use vercel_runtime::{bundled_api, run, Body, Error, Request, Response};
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    run(handler).await
-}
-
-#[bundled_api(path = "/")]
-pub async fn handler(req: Request) -> Result<Response<Body>, Error> {}
